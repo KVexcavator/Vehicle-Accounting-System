92517a7a9c47af850dc4101c4f14626568b7c181
KVexcavator <str_token@mail.ru>
2022-05-28 23:13:57 +0300

    impl admin workwlow

--- app.rb
+++ app.rb
@@ -4,29 +4,57 @@ require "./vehicle"
 
 class Vas < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  enable :sessions
+
+  YEARS = ["2000","2001","2002","2003","2004"]
+  MARKS = ["Audi","Ford","Honda"]
+  MODELS = ["TT","Q5","A8","Escape","Fiesta","Focus","Accord","Integra","S2000"]
 
   get '/' do
     @title = "Vehicle Accounting System"
+    session[:message] = "Session ready!"
     erb :index
   end
 
   get '/moderator' do
     @title = "Moderator page"
     @count = Vehicle.count
-    @years = ["2000","2001","2002","2003","2004"]
-    @marks = ["Audi","Ford","Honda"]
-    @models = ["TT","Q5","A8","Escape","Fiesta","Focus","Accord","Integra","S2000"]
     erb :moderator
   end
 
   get '/admin'  do
     @title = "Admin page"
+    @msg = session[:message]
+    @uniqum = session[:uniqum]
+    @added = session[:added]
+    @repeated = session[:repeated]
+    @vehicles = Vehicle.all
     erb :admin
   end
 
-  post '/add' do
+  post  '/add' do
     Vehicle.create(params[:vehicle])
     redirect to("/moderator")
+  end
+
+  post  '/admin/uniq' do
+    # {"mark"=>"Audi", "model"=>"TT", "prod_year"=>"2000"}
+    vehicles = Vehicle.where(mark: params[:vehicle][:mark], model: params[:vehicle][:model], prod_year: params[:vehicle][:prod_year])
+    session[:uniqum] = vehicles.count
+    redirect to("/admin")
+  end
+
+  post  '/admin/added'  do
+    num = params[:period][:days].to_i
+    period = Vehicle.where("created_at > ?", num.days.ago)
+    session[:added] = period.count
+    redirect to("/admin")
+  end
+
+  post  '/admin/repeated' do
+    repeated = Vehicle.group(:reg_number).having('count(*) > 1').order('reg_number desc').count
+    session[:repeated] = "We have a #{repeated.length} repeated auto."
+    redirect to("/admin")
   end
 
   not_found do
--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,8 @@ Dir.glob(File.join(APP_ROOT, "app", "*.rb")).each { |file| require file }
 require File.join(APP_ROOT, "config", "database")
 
 class Vas < Sinatra::Base
- set :method_override, true
- set :root, APP_ROOT
- set :views, File.join(APP_ROOT, "views")
- set :public_folder, File.join(APP_ROOT, "public")
+  set :method_override, true
+  set :root, APP_ROOT
+  set :views, File.join(APP_ROOT, "views")
+  set :public_folder, File.join(APP_ROOT, "public")
 end
